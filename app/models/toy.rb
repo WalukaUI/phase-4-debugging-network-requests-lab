768f12449b286234ff1403eea55bc398a8bdc341
WalukaUI <cwaluka@yahoo.com>
2021-08-31 17:34:20 -0700

    com

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,8 @@
 class Toy < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :name, length: { minimum: 2 }
+    validates :name, length: { maximum: 25 }
+    validates :image, presence: true
+    validates :image, length: { maximum: 250 }
 end
